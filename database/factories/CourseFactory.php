cb186c16c12b506e4b50c5953ab62ff1deb086bb
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-04-21 22:43:24 +0800

    added: name, price on model, migration, factory
    - name,price on model, migration, factory
    - added course seeder on DatabaseSeeder.php
    -  php artisan migrate:refresh --seed

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -17,7 +17,9 @@ class CourseFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->domainWord(),
+            'price' => fake()->numberBetween(5, 999)
+
         ];
     }
 }
