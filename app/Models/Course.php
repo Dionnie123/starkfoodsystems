5924c4698c90a3df4c997be9fd9781c8b0326c98
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-04-22 09:26:52 +0800

    chore: create course category
    - php artisan make:model CourseCategory --migration --factory
    - updated generated files
    - updated course model
    - added course category seeder

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Course extends Model
 {
@@ -12,5 +13,12 @@ class Course extends Model
         'name',
         'price',
         'status',
+        'category_id',
     ];
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(
+            CourseCategory::class
+        );
+    }
 }
